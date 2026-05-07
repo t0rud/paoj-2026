70948466b250c2c920b3a79776fea02c014b62d6
Tudor Bucur <tudor.bucur.05@gmail.com>
2026-05-07 15:31:55 +0300

    Lab07: exercitiile 1-2 completate

---
+++ src/com/pao/laboratory07/exercise2/ComandaRedusa.java
@@ -0,0 +1,24 @@
+package com.pao.laboratory07.exercise2;
+
+import java.util.Locale;
+
+public final class ComandaRedusa extends Comanda {
+    private double pret;
+    private int discountProcent;
+
+    public ComandaRedusa(String nume, double pret, int discountProcent) {
+        super(nume);
+        this.pret = pret;
+        this.discountProcent = discountProcent;
+    }
+
+    @Override
+    public double pretFinal() {
+        return pret * (1 - discountProcent / 100.0);
+    }
+
+    @Override
+    public String descriere() {
+        return String.format(Locale.US, "DISCOUNTED: %s, pret: %.2f lei (-%d%%) [%s]", nume, pretFinal(), discountProcent, stare);
+    }
+}
