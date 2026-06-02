4515e5c09dc42c2075e009abbbc7416f5bc47bd5
Tudor Bucur <tudor.bucur.05@gmail.com>
2026-06-02 17:47:15 +0300

    Lab7

--- src/com/pao/laboratory07/exercise1/Main.java
+++ src/com/pao/laboratory07/exercise1/Main.java
@@ -1,51 +1,49 @@
 package com.pao.laboratory07.exercise1;
 
-import com.pao.laboratory07.exercise1.exceptions.*;
+//import com.pao.laboratory07.exercise1.exceptions.CannotCancelFinalOrderException;
+//import com.pao.laboratory07.exercise1.exceptions.CannotRevertInitialOrderStateException;
+//import com.pao.laboratory07.exercise1.exceptions.OrderIsAlreadyFinalException;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        if (!scanner.hasNext()) return;
-
-        // Part A: load initial state
-        OrderState initialState = OrderState.valueOf(scanner.next());
-        Order order = new Order(initialState);
-        System.out.println(initialState); // Cerința cere doar starea, fără "Initial order state: "
-
-        while (scanner.hasNext()) {
-            OrderCommand orderCommand = OrderCommand.valueOf(scanner.next());
-            switch (orderCommand) {
-                case next -> {
-                    try {
-                        order.nextState();
-                        System.out.println(order.getCurrentState());
-                    } catch (OrderIsAlreadyFinalException e) {
-                        System.out.println("Comanda este in stare finala.");
-                    }
-                }
-                case cancel -> {
-                    try {
-                        order.cancel();
-                        System.out.println(order.getCurrentState());
-                    } catch (CannotCancelFinalOrderException e) {
-                        System.out.println("Comanda este in stare finala.");
-                    }
-                }
-                case undo -> {
-                    try {
-                        order.undoState();
-                        System.out.println(order.getCurrentState());
-                    } catch (CannotRevertInitialOrderStateException e) {
-                        System.out.println("Nu există stare anterioară pentru undo.");
-                    }
-                }
-                case QUIT -> {
-                    System.out.println("User quit the program.");
-                    return;
-                }
-            }
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        // Part A
+//        // load initial state
+//        OrderState initialState = OrderState.valueOf(scanner.next());
+//        Order order = new Order(initialState);
+//        System.out.println("Initial order state: " + initialState);
+//
+//        while (true) {
+//            OrderCommand orderCommand = OrderCommand.valueOf(scanner.next());
+//            switch (orderCommand) {
+//                case next -> {
+//                    try {
+//                        order.nextState();
+//                    } catch (OrderIsAlreadyFinalException e) {
+//                        System.out.println("Order is already in a final state.");
+//                    }
+//                }
+//                case cancel -> {
+//                    try {
+//                        order.cancel();
+//                    } catch (CannotCancelFinalOrderException e) {
+//                        System.out.println("Cannot cancel a final state order.");
+//                    }
+//                }
+//                case undo -> {
+//                    try {
+//                        order.undoState();
+//                    } catch (CannotRevertInitialOrderStateException e) {
+//                        System.out.println("Cannot undo the initial order state.");
+//                    }
+//                }
+//                case QUIT -> {
+//                    System.out.println("User quit the program.");
+//                    return;
+//                }
+//            }
+//        }
     }
 }
